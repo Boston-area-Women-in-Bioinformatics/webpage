411829efa25de3a976b8ab1147a09c1a21b6a169
sklasfeld <sjk314@gmail.com>
2024-12-18 12:43:58 -0500

    updated the social links

--- src/navigation.ts
+++ src/navigation.ts
@@ -179,11 +179,10 @@ export const footerData = {
     { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
+    { ariaLabel: 'MeetUp', icon: 'tabler:brand-meetup', href: 'https://www.meetup.com/boston-area-womens-bioinformatics/' },
+    { ariaLabel: 'Slack', icon: 'tabler:brand-slack', href: 'https://join.slack.com/t/boston-women-bioinfo/shared_invite/zt-2kg9ojwn9-arPXyQXPB_VgbVWDdoA2nQ' },
+    { ariaLabel: 'YouTube', icon: 'tabler:brand-youtube', href: 'https://m.youtube.com/channel/UCzgmQ4uoTNq_MayiwAQ448Q' },
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/Boston-area-Women-in-Bioinformatics' },
   ],
   footNote: `
     <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
