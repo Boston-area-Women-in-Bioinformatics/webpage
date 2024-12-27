53186d70259b54643c941880096aa0436836b778
sklasfeld <sjk314@gmail.com>
2024-12-27 10:53:15 -0500

    ran priettier using the command `npx prettier --write .`

--- src/config/site/blog.js
+++ src/config/site/blog.js
@@ -1,44 +1,44 @@
 // blog post configuration
 
 const blog = {
+  isEnabled: true,
+  postsPerPage: 4,
+
+  post: {
+    permalink: '/%slug%', // Variables: %slug%, %year%, %month%, %day%, %hour%, %minute%, %second%, %category%
+    noindex: false,
     isEnabled: true,
-    postsPerPage: 4,
-  
-    post: {
-      permalink: '/%slug%', // Variables: %slug%, %year%, %month%, %day%, %hour%, %minute%, %second%, %category%
-      noindex: false,
-      isEnabled: true,
-      robots: {
-        index: true,
-      },
+    robots: {
+      index: true,
     },
-  
-    list: {
-      pathname: 'blog', // Blog main path, you can change this to "articles" (/articles)
-      noindex: false,
-      isEnabled: true,
-      robots: {
-        index: true,
-      },
+  },
+
+  list: {
+    pathname: 'blog', // Blog main path, you can change this to "articles" (/articles)
+    noindex: false,
+    isEnabled: true,
+    robots: {
+      index: true,
     },
-  
-    category: {
-      pathname: 'category', // Category main path /category/some-category
-      noindex: true,
-      isEnabled: true,
-      robots: {
-        index: true,
-      },
+  },
+
+  category: {
+    pathname: 'category', // Category main path /category/some-category
+    noindex: true,
+    isEnabled: true,
+    robots: {
+      index: true,
     },
-  
-    tag: {
-      pathname: 'tag', // Tag main path /tag/some-tag
-      noindex: true,
-      isEnabled: true,
-      robots: {
-        index: false,
-      },
+  },
+
+  tag: {
+    pathname: 'tag', // Tag main path /tag/some-tag
+    noindex: true,
+    isEnabled: true,
+    robots: {
+      index: false,
     },
-  };
-  
-  export default blog;
+  },
+};
+
+export default blog;
--- src/navigation.ts
+++ src/navigation.ts
@@ -4,19 +4,19 @@ export const headerData = {
   links: [
     {
       text: 'Home',
-      href: getPermalink('')
+      href: getPermalink(''),
     },
     {
       text: 'About us',
-      href: getPermalink('/about')
+      href: getPermalink('/about'),
     },
     {
       text: 'Team',
-      href: getPermalink('/team')
+      href: getPermalink('/team'),
     },
     {
       text: 'Contact',
-      href: getPermalink('/contact')
+      href: getPermalink('/contact'),
     },
     // {
     //   text: 'Homes',
@@ -42,30 +42,30 @@ export const headerData = {
     //{
     //  text: 'Community',
     //  links: [
-        // {
-        //   text: 'Features (Anchor Link)',
-        //   href: getPermalink('/#features'),
-        // },
-        // {
-        //   text: 'Services',
-        //   href: getPermalink('/services'),
-        // },
-        // {
-        //   text: 'Pricing',
-        //   href: getPermalink('/pricing'),
-        // },
-        //{
-        //  text: 'About us',
-        //  href: getPermalink('/about'),
-        //},
-        // {
-        //   text: 'Terms',
-        //   href: getPermalink('/terms'),
-        // },
-        // {
-        //   text: 'Privacy policy',
-        //   href: getPermalink('/privacy'),
-        // },
+    // {
+    //   text: 'Features (Anchor Link)',
+    //   href: getPermalink('/#features'),
+    // },
+    // {
+    //   text: 'Services',
+    //   href: getPermalink('/services'),
+    // },
+    // {
+    //   text: 'Pricing',
+    //   href: getPermalink('/pricing'),
+    // },
+    //{
+    //  text: 'About us',
+    //  href: getPermalink('/about'),
+    //},
+    // {
+    //   text: 'Terms',
+    //   href: getPermalink('/terms'),
+    // },
+    // {
+    //   text: 'Privacy policy',
+    //   href: getPermalink('/privacy'),
+    // },
     //  ],
     //},
     // {
@@ -98,30 +98,30 @@ export const headerData = {
     //   ],
     // },
     //{
-      //text: 'Blog',
-      //href: getBlogPermalink()
-      // links: [
-      //   {
-      //     text: 'Blog List',
-      //     href: getBlogPermalink(),
-      //   },
-        // {
-        //   text: 'Article',
-        //   href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
-        // },
-        // {
-        //   text: 'Article (with MDX)',
-        //   href: getPermalink('markdown-elements-demo-post', 'post'),
-        // },
-        // {
-        //   text: 'Category Page',
-        //   href: getPermalink('tutorials', 'category'),
-        // },
-        // {
-        //   text: 'Tag Page',
-        //   href: getPermalink('astro', 'tag'),
-        // },
-      // ],
+    //text: 'Blog',
+    //href: getBlogPermalink()
+    // links: [
+    //   {
+    //     text: 'Blog List',
+    //     href: getBlogPermalink(),
+    //   },
+    // {
+    //   text: 'Article',
+    //   href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
+    // },
+    // {
+    //   text: 'Article (with MDX)',
+    //   href: getPermalink('markdown-elements-demo-post', 'post'),
+    // },
+    // {
+    //   text: 'Category Page',
+    //   href: getPermalink('tutorials', 'category'),
+    // },
+    // {
+    //   text: 'Tag Page',
+    //   href: getPermalink('astro', 'tag'),
+    // },
+    // ],
     //},
     // {
     //   text: 'Widgets',
@@ -171,10 +171,10 @@ export const footerData = {
     //    { text: 'About', href: '#' },
     //    { text: 'Blog', href: '#' },
     //    { text: 'Careers', href: '#' },
-        // { text: 'Press', href: '#' },
-        // { text: 'Inclusion', href: '#' },
+    // { text: 'Press', href: '#' },
+    // { text: 'Inclusion', href: '#' },
     //    { text: 'Social Impact', href: '#' },
-        // { text: 'Shop', href: '#' },
+    // { text: 'Shop', href: '#' },
     //  ],
     //},
   ],
@@ -183,11 +183,27 @@ export const footerData = {
     { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
   socialLinks: [
-    { ariaLabel: 'MeetUp', icon: 'tabler:brand-meetup', href: 'https://www.meetup.com/boston-area-womens-bioinformatics/' },
-    { ariaLabel: 'Slack', icon: 'tabler:brand-slack', href: 'https://join.slack.com/t/boston-women-bioinfo/shared_invite/zt-2kg9ojwn9-arPXyQXPB_VgbVWDdoA2nQ' },
-    { ariaLabel: 'LinkedIn', icon: 'tabler:brand-linkedin', href: 'https://www.linkedin.com/groups/13074137/'},
-    { ariaLabel: 'YouTube', icon: 'tabler:brand-youtube', href: 'https://m.youtube.com/channel/UCzgmQ4uoTNq_MayiwAQ448Q' },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/Boston-area-Women-in-Bioinformatics' },
+    {
+      ariaLabel: 'MeetUp',
+      icon: 'tabler:brand-meetup',
+      href: 'https://www.meetup.com/boston-area-womens-bioinformatics/',
+    },
+    {
+      ariaLabel: 'Slack',
+      icon: 'tabler:brand-slack',
+      href: 'https://join.slack.com/t/boston-women-bioinfo/shared_invite/zt-2kg9ojwn9-arPXyQXPB_VgbVWDdoA2nQ',
+    },
+    { ariaLabel: 'LinkedIn', icon: 'tabler:brand-linkedin', href: 'https://www.linkedin.com/groups/13074137/' },
+    {
+      ariaLabel: 'YouTube',
+      icon: 'tabler:brand-youtube',
+      href: 'https://m.youtube.com/channel/UCzgmQ4uoTNq_MayiwAQ448Q',
+    },
+    {
+      ariaLabel: 'Github',
+      icon: 'tabler:brand-github',
+      href: 'https://github.com/Boston-area-Women-in-Bioinformatics',
+    },
   ],
   footNote: `
     <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
