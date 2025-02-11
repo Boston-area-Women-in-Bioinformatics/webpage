fba9a6d5ff31eab24842c7ff1f65d75ea3c71dc1
sklasfeld <sjk314@gmail.com>
2025-02-10 21:41:26 -0500

    moved script to keep things organized

--- src/utils/data-utils.ts
+++
@@ -1,6 +0,0 @@
-
-import { type CollectionEntry } from 'astro:content';
-
-export function sortItemsByDateDesc(itemA: CollectionEntry<'meetups'>, itemB: CollectionEntry<'meetups'>) {
-    return new Date(itemB.data.groupByUrlname.pastEvents.edges.node.dateTime).getTime() - new Date(itemA.data.groupByUrlname.pastEvents.edges.node.dateTime).getTime();
-}
