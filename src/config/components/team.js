39cb2be465c75fb2a253de5f657bed0dfcb489c8
Yaseswini  N <yaseswini.1404@gmail.com>
2025-03-26 09:53:23 -0400

    updated team members page

--- src/config/components/team.js
+++ src/config/components/team.js
@@ -18,7 +18,7 @@ const team = {
       // Namelink: Member's Personal Website
       nameLink: 'https://www.linkedin.com/in/lpantano/',
       // Job Title
-      title: 'Co-organizer',
+      title: 'Co-chair & Founder',
       // The avatar of the first member
       avatar: '/team/lpantano.jpg',
       // Social links of the first member
@@ -32,6 +32,159 @@ const team = {
         //bluesky: 'https://bsky.app/profile/lpantano.bsky.social',
       },
     },
+
+    //Next team member
+    {
+      name: 'Yevgenia Khodor Tolan',
+      // Namelink: Member's Personal Website
+      //nameLink: '',
+      // Job Title
+      title: 'Co-Chair',
+      // The avatar of the first member
+      avatar: 'team/Yevgenia.jpg',
+      // Social links of the first member
+      social: {
+        linkedin: 'https://www.linkedin.com/in/yevgenia-khodor/',
+        //github: 'https://github.com/Alrfooh-Aysheh',
+      },
+    },
+
+    //Next team member
+    {
+      // Name: First and last name
+      name: 'Diveena Becker',
+      // Namelink: Member's Personal Website
+      nameLink: 'https://www.linkedin.com/in/diveena-becker-5945a771/',
+      // Job Title
+      title: 'Co-chair , Events committee',
+      // The avatar of the first member
+      avatar: '/team/diveena.jpg',
+      // Social links of the first member
+      social: {
+        // website: '#',
+        // twitter: 'https://x.com/lopantano',
+        // instagram: '#',
+        // facebook: '#',
+        linkedin: 'https://www.linkedin.com/in/diveena-becker-5945a771/',
+      },
+    },
+
+    //Next team member
+    {
+      // Name: First and last name
+      name: 'Lina Faller',
+      // Namelink: Member's Personal Website
+      //nameLink: '',
+      // Job Title
+      title: 'Co-Chair,Events Committee',
+      // The avatar of the first member
+      avatar: 'team/lina.jpg',
+      // Social links of the first member
+      social: {
+        // website: '#',
+        // twitter: '#',
+        // instagram: '#',
+        // facebook: '#',
+        linkedin: 'https://www.linkedin.com/in/linafaller/',
+      },
+    },
+
+    //Next team member
+    {
+      // Name: First and last name
+      name: 'Katie Hughes',
+      // Namelink: Member's Personal Website
+      nameLink: 'https://www.linkedin.com/in/hugheskj/',
+      // Job Title
+      title: 'Chair, Advisory Committee, Co-Chair, Enabling Advocacy Committee',
+      // The avatar of the first member
+      avatar: '/team/katie_sq.jpg',
+      // Social links of the first member
+      social: {
+        // website: '#',
+        // twitter: '#',
+        // instagram: '#',
+        // facebook: '#',
+      },
+    },
+
+    //Next team member
+    {
+      // Name: First and last name
+      name: 'Amulya Shastry',
+      // Namelink: Member's Personal Website
+      nameLink: 'https://ashastry2.github.io/',
+      // Job Title
+      title: 'Co-chair , Enabling Advocacy Committee',
+      // The avatar of the first member
+      avatar: '/team/AmulyaShastry.jpeg',
+      // Social links of the first member
+      social: {
+        website: 'https://ashastry2.github.io/',
+        github: 'https://github.com/Ashastry2',
+        // instagram: '#',
+        // facebook: '#',
+        linkedin: 'https://www.linkedin.com/in/amulya-shastry/',
+      },
+    },
+
+    //Next team member
+    {
+      name: 'Aysheh Alrfooh',
+      // Namelink: Member's Personal Website
+      //nameLink: '',
+      // Job Title
+      title: 'Co-chair, Sponsorship committee',
+      // The avatar of the first member
+      avatar: 'team/aysheh.jpg',
+      // Social links of the first member
+      social: {
+        linkedin: 'https://www.linkedin.com/in/aysheh-alrfooh/',
+        github: 'https://github.com/Alrfooh-Aysheh',
+      },
+    },
+
+    //Next team member
+    {
+      // Name: First and last name
+      name: 'Sakina Saif',
+      // Namelink: Member's Personal Website
+      nameLink: 'https://www.linkedin.com/in/sakinasaif/',
+      // Job Title
+      title: 'Co-chair , Communication committee',
+      // The avatar of the first member
+      avatar: '/team/sakina_sq.jpg',
+      // Social links of the first member
+      social: {
+        // website: '#',
+        // twitter: 'https://x.com/lopantano',
+        // instagram: '#',
+        // facebook: '#',
+        linkedin: 'https://www.linkedin.com/in/sakinasaif/',
+      },
+    },
+
+    //Next team member
+    {
+      // Name: First and last name
+      name: 'Shreya Rajasekar',
+      // Namelink: Member's Personal Website
+      //nameLink: '',
+      // Job Title
+      title: 'Co-chair, Communication committee',
+      // The avatar of the first member
+      avatar: 'team/shreya_sq.jpg',
+      // Social links of the first member
+      social: {
+        // website: '#',
+        // twitter: '#',
+        // instagram: '#',
+        // facebook: '#',
+        linkedin: 'https://www.linkedin.com/in/shreya-rajasekar/',
+      },
+    },
+
+    //Next team member
     {
       // The name of the first member
       name: 'Samantha Klasfeld',
@@ -52,14 +205,15 @@ const team = {
         bluesky: 'https://bsky.app/profile/sklasfeld.bsky.social',
       },
     },
-    // Yaseswini Neelamraju
+
+    // Next team member
     {
       name: 'Yaseswini Neelamraju',
       nameLink: 'https://www.linkedin.com/in/yaseswini-neelamraju-phd-14b7ba29/',
       title: 'Web Page Developer',
       avatar: '/team/yaseswini.png',
       social: {
-        // website: '#',
+        website: 'https://yaseswinineelamraju.netlify.app/',
         // twitter: '#',
         // instagram: '#',
         // facebook: '#',
@@ -67,146 +221,8 @@ const team = {
         github: 'https://github.com/Yaseswini',
       },
     },
-    {
-      // Name: First and last name
-      name: 'Diveena Becker',
-      // Namelink: Member's Personal Website
-      nameLink: 'https://www.linkedin.com/in/diveena-becker-5945a771/',
-      // Job Title
-      title: 'Co-chair of Event committee',
-      // The avatar of the first member
-      avatar: '/team/diveena.jpg',
-      // Social links of the first member
-      social: {
-        // website: '#',
-        // twitter: 'https://x.com/lopantano',
-        // instagram: '#',
-        // facebook: '#',
-        linkedin: 'https://www.linkedin.com/in/diveena-becker-5945a771/',
-      },
-    },
-    {
-      // Name: First and last name
-      name: 'Sakina Saif',
-      // Namelink: Member's Personal Website
-      nameLink: 'https://www.linkedin.com/in/sakinasaif/',
-      // Job Title
-      title: 'Co-chair of Communication committee',
-      // The avatar of the first member
-      avatar: '/team/sakina_sq.jpg',
-      // Social links of the first member
-      social: {
-        // website: '#',
-        // twitter: 'https://x.com/lopantano',
-        // instagram: '#',
-        // facebook: '#',
-        linkedin: 'https://www.linkedin.com/in/sakinasaif/',
-      },
-    },
-    {
-      // Name: First and last name
-      name: 'Amulya Shastry',
-      // Namelink: Member's Personal Website
-      nameLink: 'https://ashastry2.github.io/',
-      // Job Title
-      title: 'Co-chair of the Enabling Advocacy Committee',
-      // The avatar of the first member
-      avatar: '/team/AmulyaShastry.jpeg',
-      // Social links of the first member
-      social: {
-        website: 'https://ashastry2.github.io/',
-        github: 'https://github.com/Ashastry2',
-        // instagram: '#',
-        // facebook: '#',
-        linkedin: 'https://www.linkedin.com/in/amulya-shastry/',
-      },
-    },
-    // Next team member
-    {
-      // Name: First and last name
-      name: 'Katie Hughes',
-      // Namelink: Member's Personal Website
-      nameLink: 'https://www.linkedin.com/in/hugheskj/',
-      // Job Title
-      title: 'Chair, Advisory Committee, Co-Chair, Enabling Advocacy Committee',
-      // The avatar of the first member
-      avatar: '/team/katie_sq.jpg',
-      // Social links of the first member
-      social: {
-        // website: '#',
-        // twitter: '#',
-        // instagram: '#',
-        // facebook: '#',
-      },
-    },
-    // Next team member
-    {
-      // Name: First and last name
-      name: 'Shreya Rajasekar',
-      // Namelink: Member's Personal Website
-      //nameLink: '',
-      // Job Title
-      title: 'Communications Committe Member',
-      // The avatar of the first member
-      avatar: 'team/shreya_sq.jpg',
-      // Social links of the first member
-      social: {
-        // website: '#',
-        // twitter: '#',
-        // instagram: '#',
-        // facebook: '#',
-        linkedin: 'https://www.linkedin.com/in/shreya-rajasekar/',
-      },
-    },
-    // Next team member
-    {
-      // Name: First and last name
-      name: 'Lina Faller',
-      // Namelink: Member's Personal Website
-      //nameLink: '',
-      // Job Title
-      title: 'Co-Chair of the Events Committee',
-      // The avatar of the first member
-      avatar: 'team/lina.jpg',
-      // Social links of the first member
-      social: {
-        // website: '#',
-        // twitter: '#',
-        // instagram: '#',
-        // facebook: '#',
-        linkedin: 'https://www.linkedin.com/in/linafaller/',
-      },
-    },
+
     //Next team member
-    {
-      name: 'Aysheh Alrfooh',
-      // Namelink: Member's Personal Website
-      //nameLink: '',
-      // Job Title
-      title: 'Co-chair of the Sponsorship committee',
-      // The avatar of the first member
-      avatar: 'team/aysheh.jpg',
-      // Social links of the first member
-      social: {
-        linkedin: 'https://www.linkedin.com/in/aysheh-alrfooh/',
-        github: 'https://github.com/Alrfooh-Aysheh',
-      },
-    },
-    //Next team member
-    {
-      name: 'Yevgenia Khodor Tolan',
-      // Namelink: Member's Personal Website
-      //nameLink: '',
-      // Job Title
-      title: 'Co-Chair',
-      // The avatar of the first member
-      avatar: 'team/Yevgenia.jpg',
-      // Social links of the first member
-      social: {
-        linkedin: 'https://www.linkedin.com/in/yevgenia-khodor/',
-        //github: 'https://github.com/Alrfooh-Aysheh',
-      },
-    },
   ],
 };
 
