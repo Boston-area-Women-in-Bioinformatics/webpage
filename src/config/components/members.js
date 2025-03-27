cfd1c4afcdd844d0781590c236c41c1e4885ab4c
sklasfeld <sjk314@gmail.com>
2025-03-27 11:16:59 -0400

    add members section to teams page

---
+++ src/config/components/members.js
@@ -0,0 +1,34 @@
+// Configuration for the member component / page
+
+const members = {
+  // Whether the members page is enabled or not
+  enabled: true,
+
+  // The title of the members section
+  title: 'Our Members',
+
+  // The subtitle of the Members section
+  description: 'Meet the Members of the Boston Women in Bioinformatics',
+
+  // Member Profiles
+  members: [
+    // Isha Parikh
+    {
+      // Name: First and last name
+      name: 'Isha Parikh',
+      // Namelink: Member's Personal Website
+      //nameLink: '',
+      // Job Title
+      title: 'Member, Events Committee',
+      // Leadership team member Photo
+      avatar: 'team/isha.jpeg',
+      // Social links
+      social: {
+        linkedin: 'https://www.linkedin.com/in/isha2106/',
+        github: 'https://github.com/isha2106',
+      },
+    },
+  ],
+};
+
+export default members;
