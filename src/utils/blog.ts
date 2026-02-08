39cb41731ee359fb6cb5e36b2edecc82dd81c9e8
sklasfeld <sjk314@gmail.com>
2026-02-07 21:57:29 -0500

    ran prettier

--- src/utils/blog.ts
+++ src/utils/blog.ts
@@ -3,7 +3,15 @@ import { getCollection } from 'astro:content';
 import type { CollectionEntry } from 'astro:content';
 import type { Post } from '~/types';
 import { APP_BLOG } from 'astrowind:config';
-import { cleanSlug, trimSlash, BLOG_BASE, POST_PERMALINK_PATTERN, CATEGORY_BASE, TAG_BASE, SERIES_BASE } from './permalinks';
+import {
+  cleanSlug,
+  trimSlash,
+  BLOG_BASE,
+  POST_PERMALINK_PATTERN,
+  CATEGORY_BASE,
+  TAG_BASE,
+  SERIES_BASE,
+} from './permalinks';
 
 const generatePermalink = async ({
   id,
